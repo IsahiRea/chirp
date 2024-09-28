2b63c7097ec045d4101730381d4113e59f3804ea
IsahiRea <Isahi.rea@live.com>
2024-09-27 20:12:19 -0500

    Serve Image

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "net/http"
 func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/assets/logo", http.FileServer(http.Dir("logo.png")))
 
 	server := &http.Server{
 		Addr:    ":8080",
